2068021bd430a939b0b38cfd60fb4518d7845363
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2022-12-27 02:13:33 +0200

    task-01.js-ready

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,2 +1,14 @@
-const list = document.querySelectorAll("#categories");
-console.log(`Number of categories:`, categories.children.length);
+const categories = document.querySelector("#categories");
+// console.log(categories);
+
+const showNumbersCategory = () => {
+  console.log(`Number of categories:`, categories.children.length);
+};
+showNumbersCategory();
+
+const titleEl = document.querySelectorAll(".item");
+// console.log(titleEl);
+titleEl.forEach(function (category) {
+  console.log(`Category:`, category.firstElementChild.textContent);
+  console.log(`Elements:`, category.querySelectorAll("li").length);
+});
