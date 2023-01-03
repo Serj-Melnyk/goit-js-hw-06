0db1a2bbbe2932fd150af1e5783dc06d11cb2eb7
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-03 17:38:02 +0200

    task-06.js 6-ok

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -6,16 +6,9 @@ function onInput(evt) {
   const { value } = evt.currentTarget;
   console.log(value);
 
-  if (value.length === Number(input.dataset.length)) {
-    input.classList.add("valid");
-  } else if (value.length !== Number(input.dataset.length)) {
+  if (value.length !== Number(input.dataset.length)) {
     input.classList.add("invalid");
-    input.classList.remove("valid");
-  } else if (value === "") {
-    input.classList = "";
-    // input.classList.add("invalid");
-    // input.classList.remove("valid");
-  }
+  } else input.classList.replace("invalid", "valid");
 
   console.log(input.classList);
 }
