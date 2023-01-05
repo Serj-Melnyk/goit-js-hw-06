1b744ffe2fbde82f7e365e4b2a50e3b24483a765
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-05 02:13:36 +0200

    hw-06.js - 10not ok

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -2,50 +2,77 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const control = document.querySelector("#controls");
-// console.log(control);
+// const control = document.querySelector("#controls");
 
-const input = document.querySelector("input");
-// console.log(input);
+// const input = document.querySelector("input");
+// // console.log(input);
 
-input.addEventListener("input", onInput);
-function onInput(evt) {
-  const { value } = evt.currentTarget;
+// input.addEventListener("input", onInput);
+// function onInput(evt) {
+//   // evt.currentTarget = createBoxes(amounte);
+//   const { value } = evt.currentTarget;
+//   let amounte = 0;
+//   for (let i = 1; i <= value; i += 1) {
+//     amounte += `${value}`;
+//     createBoxes(value);
+//     console.log(amounte);
+//     // return onClick(evt);
+//   }
+// }
 
-  createBoxes(value) = `${value}`;
-  console.log(value);
-}
+// const box = document.querySelector("#boxes");
+// box.addEventListener("click", onBox);
+// function onBox(evt) {
+//   box.reset();
+// }
 
-const box = document.querySelector("#boxes");
-// console.log(box);
-box.addEventListener("create", onCreate);
-function onCreate(evt) {}
+// const buttonCreate = document.querySelector("button[data-create]");
 
+// buttonCreate.addEventListener("click", onClick);
+
+// const div = document.createElement("div");
+// let divCnange = 30;
+
+// function onClick(evt) {
+//   divCnange += 10;
+//   div.style.width = `${divCnange}px`;
+//   div.style.height = `${divCnange}px`;
+
+//   div.style.backgroundColor = `${getRandomHexColor()}`;
+
+//   box.append(div);
+// }
+
+// const buttonDestroy = document.querySelector("button[data-destroy]");
+// // console.log(buttonDestroy);
+
+// buttonDestroy.addEventListener("click", destroyBoxes);
+// function destroyBoxes(evt) {
+//   div.remove();
+// }
+//..........................................................
+// function reset() {
+//   div.remove();
+// }
+// function createBoxes(amounte) {
+//   for (let i = 1; i <= input.value; i += 1) {
+//     amounte = input.value;
+//     // console.log(amounte);
+//   }
+// }
+// // createBoxes(value);
+// // evt.currentTarget.reset();
+// function createBoxes(amount) {
+//   div = amount;
+// }
+//......................................................
 const buttonCreate = document.querySelector("button[data-create]");
-// console.log(buttonCreate);
-buttonCreate.addEventListener("click", onClick);
-
-const div = document.createElement("div");
-function onClick(evt) {
-  div.style.width = "30px";
-  div.style.height = "30px";
-  div.style.backgroundColor = `${getRandomHexColor()}`;
-  box.append(div);
-
-  // console.log(evt);
+console.log(buttonCreate);
+buttonCreate.addEventListener("click", onCreate);
+function onCreate(evt) {
+  let div = [];
+  for (let i = 1; i <= input.value; i += 1) {
+    div +=
+  }
+  console.log(evt.currentTarget);
 }
-
-const buttonDestroy = document.querySelector("button[data-destroy]");
-console.log(buttonDestroy);
-
-buttonDestroy.addEventListener("click", destroyBoxes);
-function destroyBoxes(evt) {
-  div.remove();
-  // console.log(evt.currentTarget);
-}
-
-function createBoxes(value) {
-  onClick()
-}
-
-// box.reset();
